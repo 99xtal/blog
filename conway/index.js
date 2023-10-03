f44ca88e5856ceff505e509ad42bb62c9ff32b09
jrybarczyk <jrybarczyk@exclusiveresorts.com>
2023-10-02 23:27:13 -0400

    feat(conway): prototype Game of Life simulator using canvas API

---
+++ conway/index.js
@@ -0,0 +1,173 @@
+'use strict';
+
+const CELL_COLOR = 'black';
+const CELL_SIZE = 15;
+const LINE_COLOR = 'gray';
+
+let state = {};
+let timerId = null;
+let running = false;
+
+function drawGrid(ctx, options = {
+    cellSize: CELL_SIZE,
+    lineColor: LINE_COLOR,
+}) {
+    const canvasHeight = ctx.canvas.height;
+    const canvasWidth = ctx.canvas.width;
+    ctx.strokeStyle = options.lineColor;
+    for (let y = 0; y <= canvasHeight; y += options.cellSize) {
+        for (let x = 0; x <= canvasWidth; x += options.cellSize) {
+            ctx.strokeRect(x, y, options.cellSize, options.cellSize);
+        }
+    }
+    ctx.save();
+}
+
+function resetGameState() {
+    state = {};
+    if (timerId) {
+        clearInterval(timerId);
+        btn.innerHTML = "Start"
+    }
+}
+
+function toggleStart() {
+    running = !running;
+    const btn = document.getElementById('startstop');
+    if (running) {
+        timerId = setInterval(() => {
+            state = createNextFrame(state);
+        }, 60);
+        btn.innerHTML = "Stop"
+    } else if (timerId) {
+        clearInterval(timerId);
+        btn.innerHTML = "Start"
+    }
+}
+
+function draw() {
+    const canvas = document.getElementById('game');
+    const ctx = canvas.getContext('2d');
+    drawGrid(ctx);
+}
+
+function getMouseCoordinate(e) {
+    const x = Math.floor(e.offsetX / CELL_SIZE);
+    const y = Math.floor(e.offsetY / CELL_SIZE);
+    return [x, y];
+}
+
+function fillCell(ctx, coordinate) {
+    const canvasX = coordinate[0] * CELL_SIZE;
+    const canvasY = coordinate[1] * CELL_SIZE;
+    ctx.fillStyle = CELL_COLOR;
+    ctx.fillRect(canvasX, canvasY, CELL_SIZE, CELL_SIZE);
+}
+
+function clearCell(ctx, coordinate) {
+    const canvasX = coordinate[0] * CELL_SIZE;
+    const canvasY = coordinate[1] * CELL_SIZE;
+    ctx.clearRect(canvasX, canvasY, CELL_SIZE, CELL_SIZE);
+    ctx.strokeRect(canvasX, canvasY, CELL_SIZE, CELL_SIZE);
+}
+
+function key(coordinate) {
+    return `${coordinate[0]},${coordinate[1]}`;
+}
+
+function coordinateFromKey(key) {
+    return key.split(',').map((k) => parseInt(k, 10));
+}
+
+function getNeighbors(coordinates) {
+    const [x, y] = coordinates;
+    const neighbors = [];
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (i === x && j === y) {
+                continue;
+            }
+            neighbors.push([i, j]);
+        }
+    }
+    return neighbors;
+}
+
+function createNextFrame(currentState) {
+    const nextState = { ...currentState };
+    const currentAliveCells = Object.entries(currentState).filter(([_, alive]) => alive).map(([k]) => coordinateFromKey(k));
+
+    for (const cell of currentAliveCells) {
+        const neighbors = getNeighbors(cell);
+        for (const neighbor of neighbors) {
+            if (!nextState[key(neighbor)]) {
+                nextState[key(neighbor)] = false;
+            }
+        }
+    }
+
+    for (const cell of Object.keys(nextState).map(coordinateFromKey)) {
+        const neighbors = getNeighbors(cell);
+        let numAliveNeighbors = 0
+        for (const n of neighbors) {
+            if (currentState[key(n)]) {
+                numAliveNeighbors += 1;
+            }
+        }
+        
+        const cellKey = key(cell);
+        if (currentState[cellKey] === true) {
+            if (numAliveNeighbors < 2) {
+                nextState[cellKey] = false;
+                continue;
+            } else if (numAliveNeighbors > 3) {
+                nextState[cellKey] = false;
+                continue;
+            } else {
+                nextState[cellKey] = currentState[cellKey]
+                continue;
+            }
+        } else if (!currentState[cellKey] && numAliveNeighbors === 3) {
+            nextState[cellKey] = true
+            continue;
+        }
+    }
+
+    return nextState;
+}
+
+function draw() {
+    const ctx = document.getElementById('game').getContext('2d');
+    
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    drawGrid(ctx);
+
+    for (const [key, alive] of Object.entries(state)) {
+        const coordinate = coordinateFromKey(key);
+        if (alive) {
+            fillCell(ctx, coordinate);
+        } else {
+            clearCell(ctx, coordinate);
+        }
+    }
+    
+    window.requestAnimationFrame(draw);
+}
+
+function init() {
+    const canvas = document.getElementById('game');
+
+    canvas.addEventListener('mousedown', (e) => {
+        const coordinates = getMouseCoordinate(e);
+        const cellState = state[key(coordinates)];
+        if (!cellState) {
+            state[key(coordinates)] = true;
+        } else {
+            state[key(coordinates)] = false;
+        }
+    })
+
+    window.requestAnimationFrame(draw);
+}
+
+init();
